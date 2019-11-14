845eacd6ae221b20694d8f8027efd49edd10ce7e
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-15 04:20:14 +0900

    Initial Commit

---
+++ kodomo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kodomo
+//
+//  Created by Kei Sugano on 2019/11/15.
+//  Copyright © 2019 Kei Sugano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
