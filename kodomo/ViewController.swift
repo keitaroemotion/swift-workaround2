1248d190d678a90513b5a561d210254f5000c483
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-15 04:58:22 +0900

    location add

--- kodomo/ViewController.swift
+++ kodomo/ViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     // let button = UIButton()
-
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
